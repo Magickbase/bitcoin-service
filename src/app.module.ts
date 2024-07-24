5177443b1729ca6816650033f96f47a9ecc74edf
daryl <sai9375@hotmail.com>
2024-07-24 16:30:32 +0800

    test1

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { ExplorerService } from './services/explorer.service';
 import { NervosService } from './services/nervos.service';
 import { SyncLogger } from './logger/sync.logger';
 import { SyncPreviousService } from './services/sync_previous.service';
+import { SyncPreviousLogger } from './logger/sync-previous.logger';
+import { BitcoinBlockstream } from './services/bitcoin-blockstream.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ load: [config] })],
   controllers: [],
-  providers: [BitcoinService, SyncService, ExplorerService, NervosService, SyncLogger, SyncPreviousService],
+  providers: [BitcoinService, SyncService, ExplorerService, NervosService, SyncLogger, SyncPreviousService, SyncPreviousLogger, BitcoinBlockstream],
 })
 export class AppModule { }
